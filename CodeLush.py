80bc8b7a26f3303d3a9f6c61fe1f1eaddfc6865f
Detroit2 <75081267+Detroit2@users.noreply.github.com>
2020-12-07 10:11:50 +0530

    Add files via upload

--- CodeLush.py
+++ CodeLush.py
@@ -82,7 +82,7 @@ async def help(ctx):
 @client.command()
 async def modHelp(ctx):
 	mod = discord.Embed(tittle="mod", color=ctx.author.color)
-	mod.add_field(name="Moderation Command Menu", value="```!clear (ammount) : Deletes the specified ammount of messages from the channel```\n```!ban (user) (reasion) : Bans the specified user from the server```\n```!kick (user) (reason) : Kicks the specified user from the server```\n```mute (user) (reason) : Mutes the specified user from the server```\n```unmute (user) : Unmutes the specified user```\n")
+	mod.add_field(name="Moderation Command Menu", value="```>clear (ammount) : Deletes the specified ammount of messages from the channel```\n```>ban (user) (reasion) : Bans the specified user from the server```\n```>kick (user) (reason) : Kicks the specified user from the server```\n```>mute (user) (reason) : Mutes the specified user from the server```\n```>unmute (user) : Unmutes the specified user```\n```>announce (message) : Sends a announcemnt message in a embed style```\n")
 	mod.set_footer(text="More moderation commands will be added soon")
 	await ctx.send(embed=modHelp)
 
@@ -90,7 +90,9 @@ async def modHelp(ctx):
 @client.command()
 async def miscHelp(ctx):
 	misc = discord.Embed(tittle="misc", color=ctx.author.color)
-	misc.add_field(name="Miscellaneous Command Menu", value="```!ping : Tells the bot's latency```\n```!8ball (question) : Tells the answer of the asked question in a random yes/no answer```\n```!meme : Send a hot meme from reddit```\n```")
+	misc.add_field(name="Miscellaneous Command Menu", value="```>ping : Tells the bot's latency```\n```>8ball (question) : Tells the answer of the asked question in a random yes/no answer```\n```>meme : Send a hot meme from reddit```\n```")
+	misc.set_footer(text="More miscellaneous commands will be added soon")
+	await ctx.send(embed=misc)
 
 #ban command
 @client.command(aliases=['b'])
@@ -168,11 +170,10 @@ async def kill(ctx, user):
 #announcemnt command
 @client.command(aliases=["ann"])
 @commands.has_permissions(administrator=True, manage_messages=True, manage_roles=True, ban_members=True, kick_members=True)
-async def announce(ctx, message):
+async def announce(ctx,*,message):
 	anno = discord.Embed(tittle="ann", color=ctx.author.color)
 	anno.add_field(name="Announcement", value=message)
 	anno.set_footer(text=f"Announcement by {ctx.author.name}")
 	await ctx.send(embed=anno)
-	await ctx.send('@everyone', delete_after=3) 
 
 client.run(os.environ['DISCORD_TOKEN'])
