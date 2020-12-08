342194be4d347c42d495a6f7f9f383653ee71965
LucasWong080917 <74125180+LucasWong080917@users.noreply.github.com>
2020-12-08 10:47:30 +0800

    Update CodeLush.py

--- CodeLush.py
+++ CodeLush.py
@@ -12,104 +12,134 @@ import typing
 
 intents = discord.Intents.all()
 prefixes = [">"]
-client = commands.Bot(command_prefix=list(prefixes),intents = intents)
+client = commands.Bot(command_prefix=list(prefixes), intents=intents)
 
-client = commands.Bot(command_prefix = prefixes)
+client = commands.Bot(command_prefix=prefixes)
 
 status = ['Listening to >help', 'Make sure to read the rules!']
 
 client.remove_command("help")
 
+
 @client.event
 async def on_ready():
-	change_status.start()
-	print('Bot is ready.')
+    change_status.start()
+    print('Bot is ready.')
+
 
 @tasks.loop(seconds=20)
 async def change_status():
-	await client.change_presence(activity=discord.Game(choice(status)))
+    await client.change_presence(activity=discord.Game(choice(status)))
+
 
 #ping command
 @client.command()
 async def ping(ctx):
-	await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
 
 #clear command
 @client.command(aliases=["cls", "purge"])
 @commands.has_permissions(manage_messages=True, administrator=True)
-async def clear(ctx, ammount:int):
-	await ctx.channel.purge(limit=ammount+1)
-	await ctx.send(f'I have deleted {ammount} of messages', delete_after=5)
-	return
+async def clear(ctx, ammount: int):
+    await ctx.channel.purge(limit=ammount + 1)
+    await ctx.send(f'I have deleted {ammount} of messages', delete_after=5)
+    return
+
 
 #8ball command
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, question):
-	import random
-	responses = ["It is certain.",
-				"It is decidedly so.",
-				"Without a doubt.",
-				"Yes - definitely.",
-				"You may rely on it.",
-				"As I see it, yes.",
-				"Most likely.",
-				"Outlook good.",
-				"Yes.",
-				"Signs point to yes.",
-				"Reply hazy, try again.",
-				"Ask again later.",
-				"Better not tell you now.",
-				"Cannot predict now.",
-				"Concentrate and ask again.",
-				"Don't count on it.",
-				"My reply is no.",
-				"My sources say no.",
-				"Outlook not so good.",
-				"Very doubtful."]
-	await ctx.send(f'{random.choice(responses)}')
+    import random
+    responses = [
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful.",]
+    await ctx.send(f'{random.choice(responses)}')
+
 
 #help command
 @client.command(aliases=['h'])
 async def help(ctx):
-	helpEmbed = discord.Embed(tittle="Help Menu", color=ctx.author.color)
-	helpEmbed.set_author(name="Help Menu:\nPrefix = '!'")
-	helpEmbed.add_field(name="Moderation Command Menu", value="```Type .modHelp to open that```", inline=True)
-	helpEmbed.add_field(name="Miscellaneous Command Menu", value="```Type .miscHelp to open that```", inline=True)
+    helpEmbed = discord.Embed(tittle="Help Menu", color=ctx.author.color)
+    helpEmbed.set_author(name="Help Menu:\nPrefix = '!'")
+    helpEmbed.add_field(
+        name="Moderation Command Menu",
+        value="```Type .modHelp to open that```",
+        inline=True)
+    helpEmbed.add_field(
+        name="Miscellaneous Command Menu",
+        value="```Type .miscHelp to open that```",
+        inline=True)
 
-	await ctx.send(embed=helpEmbed)
+    await ctx.send(embed=helpEmbed)
+
 
 #modHelp
 @client.command()
 async def modHelp(ctx):
-	mod = discord.Embed(tittle="mod", color=ctx.author.color)
-	mod.add_field(name="Moderation Command Menu", value="```!clear (ammount) : Deletes the specified ammount of messages from the channel```\n```!ban (user) (reasion) : Bans the specified user from the server```\n```!kick (user) (reason) : Kicks the specified user from the server```\n```mute (user) (reason) : Mutes the specified user from the server```\n```unmute (user) : Unmutes the specified user```\n")
-	mod.set_footer(text="More moderation commands will be added soon")
-	await ctx.send(embed=modHelp)
+    mod = discord.Embed(tittle="mod", color=ctx.author.color)
+    mod.add_field(
+        name="Moderation Command Menu",
+        value=
+        "```!clear (ammount) : Deletes the specified ammount of messages from the channel```\n```!ban (user) (reasion) : Bans the specified user from the server```\n```!kick (user) (reason) : Kicks the specified user from the server```\n```mute (user) (reason) : Mutes the specified user from the server```\n```unmute (user) : Unmutes the specified user```\n"
+    )
+    mod.set_footer(text="More moderation commands will be added soon")
+    await ctx.send(embed=modHelp)
+
 
 #miscHelp
 @client.command()
 async def miscHelp(ctx):
-	misc = discord.Embed(tittle="misc", color=ctx.author.color)
-	misc.add_field(name="Miscellaneous Command Menu", value="```!ping : Tells the bot's latency```\n```!8ball (question) : Tells the answer of the asked question in a random yes/no answer```\n```!meme : Send a hot meme from reddit```\n```")
+    misc = discord.Embed(tittle="misc", color=ctx.author.color)
+    misc.add_field(
+        name="Miscellaneous Command Menu",
+        value=
+        "```!ping : Tells the bot's latency```\n```!8ball (question) : Tells the answer of the asked question in a random yes/no answer```\n```!meme : Send a hot meme from reddit```\n```"
+    )
+
 
 #ban command
 @client.command(aliases=['b'])
 @commands.has_permissions(ban_members=True, administrator=True)
 async def ban(ctx, member: discord.Member, *, reason="No reason provided"):
-	await ctx.send(f'Banned {member} from the server.')
-	await member.ban(reason=reason)
+    await ctx.send(f'Banned {member} from the server.')
+    await member.ban(reason=reason)
+
 
 #kick command
 @client.command(aliases=['k'])
 @commands.has_permissions(kick_members=True, administrator=True)
 async def kick(ctx, member: discord.Member, *, reason="No reason provided"):
-	await ctx.send(f'Kicked {member} from the server.')
-	await member.kick(reason=reason)
+    await ctx.send(f'Kicked {member} from the server.')
+    await member.kick(reason=reason)
+
 
 #mute command
 @client.command()
-@commands.has_permissions(kick_members=True, manage_messages=True, administrator=True, manage_roles=True)
-async def mute(ctx, member: discord.Member, mute_time : int, *, reason=None):
+@commands.has_permissions(
+    kick_members=True,
+    manage_messages=True,
+    administrator=True,
+    manage_roles=True)
+async def mute(ctx, member: discord.Member, mute_time: int, *, reason=None):
     if not member:
         await ctx.send("Who do you want me to mute?")
         return
@@ -119,7 +149,12 @@ async def mute(ctx, member: discord.Member, mute_time : int, *, reason=None):
         await ctx.guild.create_role(name='Muted')
 
     for channel in ctx.guild.channels:
-            await channel.set_permissions(role, speak=False, send_messages=False, read_message_history=True, read_messages=True)
+        await channel.set_permissions(
+            role,
+            speak=False,
+            send_messages=False,
+            read_message_history=True,
+            read_messages=True)
 
     await member.add_roles(role)
     await ctx.send(f"{member.mention} was muted for {reason}")
@@ -130,39 +165,53 @@ async def mute(ctx, member: discord.Member, mute_time : int, *, reason=None):
     await ctx.send(f"{member.mention} was unmuted")
     await member.send(f"You were unmuted in **{ctx.guild}**")
 
+
 #unmute command
 @client.command()
 @commands.has_permissions(manage_roles=True, administrator=True)
 async def unmute(ctx, member: discord.Member):
-	mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
-	await member.remove_roles(mutedRole)
-	await ctx.send(f'Unmuted {ctx.members.mention}')
-	await member.send(f'You have been unmuted from the server {ctx.guild.name}')
+    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+    await member.remove_roles(mutedRole)
+    await ctx.send(f'Unmuted {ctx.members.mention}')
+    await member.send(f'You have been unmuted from the server {ctx.guild.name}'
+                      )
+
 
 #meme command
 @client.command()
 async def meme(ctx):
     async with aiohttp.ClientSession() as cs:
-        async with cs.get ('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+        async with cs.get(
+                'https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
             res = await r.json()
-            embed = discord.Embed(title = "Memes", color = discord.Color.orange())
-            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            embed = discord.Embed(title="Memes", color=discord.Color.orange())
+            embed.set_image(url=res['data']['children'][random.randint(0, 25)]
+                            ['data']['url'])
             await ctx.send(embed=embed)
-            
+
+
 #kill command
 @client.command()
 async def kill(ctx, user):
-	k = random.randint(0,5)
-	if k == 0:
-		await ctx.send(f'You challenged {user} to a fist fight to the death. You won.')
-	if k == 2:
-		await ctx.send(f'{user} had a mid air collision with nyan-cat')
-	if k == 3:
-		await ctx.send(f'{user} fell down a cliff while playing Pokemon Go. Good job on keeping your nose in that puny phone. :iphone:')
-	if k == 4:
-		await ctx.send(f"{user} presses a random button and is teleported to the height of 100m, allowing them to fall to their inevitable death.\nMoral of the story: Don't go around pressing random buttons.")
-	if k == 5:
-		await ctx.send(f'{user} is sucked into Minecraft. {user}, being a noob at the so called Real-Life Minecraft faces the Game Over screen.')
+    k = random.randint(0, 5)
+    if k == 0:
+        await ctx.send(
+            f'You challenged {user} to a fist fight to the death. You won.')
+    if k == 2:
+        await ctx.send(f'{user} had a mid air collision with nyan-cat')
+    if k == 3:
+        await ctx.send(
+            f'{user} fell down a cliff while playing Pokemon Go. Good job on keeping your nose in that puny phone. :iphone:'
+        )
+    if k == 4:
+        await ctx.send(
+            f"{user} presses a random button and is teleported to the height of 100m, allowing them to fall to their inevitable death.\nMoral of the story: Don't go around pressing random buttons."
+        )
+    if k == 5:
+        await ctx.send(
+            f'{user} is sucked into Minecraft. {user}, being a noob at the so called Real-Life Minecraft faces the Game Over screen.'
+        )
+
 
 #welcome commands
 class Welcome(commands.Cog):
@@ -171,40 +220,58 @@ class Welcome(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel = self.bot.get_channel(id = 783298898194202665)
+        channel = self.bot.get_channel(id=783306605466746881)
 
         if member.bot == True:
-            botrole = discord.utils.get(member.guild.roles, name = 'Members Bots',)
+            botrole = discord.utils.get(
+                member.guild.roles, name="Members Bots"
+            )
             await member.add_roles(botrole)
 
         else:
-            verifiedrole = discord.utils.get(member.guild.roles, name = 'Verified')
+            verifiedrole = discord.utils.get(
+                member.guild.roles, name='Verified')
             await member.add_roles(verifiedrole)
 
-            em=discord.Embed(title=f"Hi{member.name}, Welcome to CodeLush.", description="Thanks for joining this server, we are {len(list(member.guild.members))} members now!", color=discord.Color.green())
+            em = discord.Embed(
+                title=f"Hi{member.name}, Welcome to CodeLush.",
+                description=
+                "Thanks for joining this server, we are {len(list(member.guild.members))} members now!",
+                color=discord.Color.green())
 
             await channel.send(embed=em)
             await member.send(embed=em)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        channel = self.bot.get_channel(id = 783631786575659028)
+        channel = self.bot.get_channel(id=783306605466746881)
 
-        em = discord.Embed(title = f"It seems like {member.name} just left us", description = f"We are only {len(list(member.guild.members))} now :sob:", color = discord.Color.red())
+        em = discord.Embed(
+            title=f"It seems like {member.name} just left us",
+            description=
+            f"We are only {len(list(member.guild.members))} now :sob:",
+            color=discord.Color.red())
 
         await channel.send(embed=em)
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
 
+
 #announcemnt command
 @client.command(aliases=["ann"])
-@commands.has_permissions(administrator=True, manage_messages=True, manage_roles=True, ban_members=True, kick_members=True)
+@commands.has_permissions(
+    administrator=True,
+    manage_messages=True,
+    manage_roles=True,
+    ban_members=True,
+    kick_members=True)
 async def announce(ctx, message):
-	anno = discord.Embed(tittle="ann", color=ctx.author.color)
-	anno.add_field(name="Announcement", value=message)
-	anno.set_footer(text=f"Announcement by {ctx.author.name}")
-	await ctx.send(embed=anno)
-	await ctx.send('@everyone', delete_after=3) 
+    anno = discord.Embed(tittle="ann", color=ctx.author.color)
+    anno.add_field(name="Announcement", value=message)
+    anno.set_footer(text=f"Announcement by {ctx.author.name}")
+    await ctx.send(embed=anno)
+    await ctx.send('@everyone', delete_after=3)
+
 
 client.run(os.environ['DISCORD_TOKEN'])
