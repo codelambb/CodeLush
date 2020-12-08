8461aa709015008a70c89b95955f410d2bec1107
Detroit2 <75081267+Detroit2@users.noreply.github.com>
2020-12-08 23:00:46 +0530

    Add files via upload

--- CodeLush.py
+++ CodeLush.py
@@ -37,6 +37,7 @@ async def change_status():
 #auto role
 @client.event
 async def on_member_join(member):
+  print("Someone joined")
   notrole=discord.utils.get(member.server.roles, name='Not Verified')
   await member.add_roles(notrole)
 
