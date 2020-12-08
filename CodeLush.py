5d190bee6bf096e26c15067e9e28c1c22a1913f7
Detroit2 <75081267+Detroit2@users.noreply.github.com>
2020-12-08 09:23:55 +0530

    Add files via upload

--- CodeLush.py
+++ CodeLush.py
@@ -89,6 +89,9 @@ async def help(ctx):
         name="Miscellaneous Command Menu",
         value="```Type >miscHelp to open that```",
         inline=True)
+    helpEmbed.set_image(
+      url="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/67401945-34fc-46b8-8e8f-1982847277d4/ddba22b-2fad9d00-1d3f-4ec8-a65d-199a09dfa4e1.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNjc0MDE5NDUtMzRmYy00NmI4LThlOGYtMTk4Mjg0NzI3N2Q0XC9kZGJhMjJiLTJmYWQ5ZDAwLTFkM2YtNGVjOC1hNjVkLTE5OWEwOWRmYTRlMS5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ._-whxwEBEaTLWUvSWL80KTGiwpoy9dSPzXSRhfTAzeM"
+    )
 
     await ctx.send(embed=helpEmbed)
 
